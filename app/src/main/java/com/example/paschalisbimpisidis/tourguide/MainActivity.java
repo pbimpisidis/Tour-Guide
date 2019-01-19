b0cf73635e9698a1ace332a706a4264df6f9c2e3
paschalisbimpisidis <pbimpisidis@gmail.com>
2019-01-19 21:33:09 +0100

    add implementation 'com.android.support:design:28.0.0' to gradle file to support tabLayout

--- app/src/main/java/com/example/paschalisbimpisidis/tourguide/MainActivity.java
+++ app/src/main/java/com/example/paschalisbimpisidis/tourguide/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.paschalisbimpisidis.tourguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+        // link the viewPager with the tabLayout
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
