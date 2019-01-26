a7b3d4214b206f1c62883edc48e36661c9203a55
paschalisbimpisidis <pbimpisidis@gmail.com>
2019-01-26 15:14:57 +0100

    add cafe fragment

--- app/src/main/java/com/example/paschalisbimpisidis/tourguide/CafesFragment.java
+++ app/src/main/java/com/example/paschalisbimpisidis/tourguide/CafesFragment.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RestaurantFragment extends Fragment {
+public class CafesFragment extends Fragment {
 
-    public RestaurantFragment() {
+    public CafesFragment() {
         // Required empty public constructor
     }
 
@@ -26,13 +26,12 @@ public class RestaurantFragment extends Fragment {
 
         // Create an array of words
         final ArrayList<Attraction> attractions = new ArrayList<>();
-        attractions.add(new Attraction("Ecco Zurich", "Doeltschiweg 234", "Wed-Sun 7pm-12am"));
-        attractions.add(new Attraction("Buckhuser", "Buckhauserstrasse 34", "6:30am–12am"));
-        attractions.add(new Attraction("Da Angela", "Hohlstrasse 449", "Mo-Sa 6:30am–12am"));
-        attractions.add(new Attraction("Haus zum Rüden", "Limmatquai 42", "Mo-Sa 10:30am–11:30pm"));
-        attractions.add(new Attraction("Lindenhofkeller", "Pfalzgasse 4", "Mo-Fr 11:45am–11:45pm"));
-        attractions.add(new Attraction("BÜ’s", "Kuttelgasse 15", "Mo-Fr 11am–11:30pm"));
-        attractions.add(new Attraction("Mythos", "Sihlstrasse 93", "Mo-Fr 7am–7pm"));
+        attractions.add(new Attraction("Roots", "Lintheschergasse 15", "7am-8pm"));
+        attractions.add(new Attraction("Juicery 21", "Sihlstrasse 93", "7am–7pm"));
+        attractions.add(new Attraction("Boreal", "Talacker 41", "9am–6:30pm"));
+        attractions.add(new Attraction("Mame", "Josefstrasse 160", "Mo-Sa 8:30am–6pm"));
+        attractions.add(new Attraction("eNDi", "Zypressenstrasse 49", "Mo-Sa 7am–10pm"));
+        attractions.add(new Attraction("Coffee Shack", "Mullerstrasse 31", "Mo-Sa 8am–5:30pm"));
 
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
@@ -40,7 +39,7 @@ public class RestaurantFragment extends Fragment {
         // simple_list_item_1.xml layout resource defined in the Android framework.
         // This list item layout contains a single {@link TextView}, which the adapter will set to
         // display a single attraction.
-        AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions, R.color.restaurants);
+        AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions, R.color.cafes);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
